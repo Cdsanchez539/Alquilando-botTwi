d92c8710970a67ebfa409262055557ebde3406ad
Cdsanchez539 <csancheza34@gmail.com>
2025-07-24 21:28:03 -0500

    Update handler.php

--- api/handler.php
+++ api/handler.php
@@ -1,7 +1,7 @@
 <?php
 
 // --- SCRIPT DE DIAGNÓSTICO DE RUTA ---
-// Esta es la prueba más simple posible. Si podemos ver este mensaje en un navegador,
+// Si podemos ver este mensaje en un navegador,
 // significa que nuestra URL es correcta y Vercel está funcionando.
 
 echo "El Handler del Bot Alquilando está en línea y funcionando. Si ves este mensaje, la conexión desde Twilio debería funcionar.";
