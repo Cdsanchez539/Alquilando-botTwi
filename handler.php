0080761b971c2479a1ff8902f52a1215d0f6cfb8
Cdsanchez539 <csancheza34@gmail.com>
2025-07-24 21:46:39 -0500

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,9 +1,6 @@
 <?php
 
 // --- SCRIPT DE DIAGNÓSTICO DE RUTA ---
-// Si podemos ver este mensaje en un navegador,
-// significa que nuestra URL es correcta y Vercel está funcionando.
-
 echo "El Handler del Bot Alquilando está en línea y funcionando. Si ves este mensaje, la conexión desde Twilio debería funcionar.";
 
 ?>
